7cf2a07094989616ed78ed898284bb2f5a1d84c5
Alvaro Frias Garay <alvarofriasgaray@gmail.com>
2021-06-09 16:13:45 -0300

    Added a different text editor for instructions to set up stacks Added functionality to add cards to stacks

--- src/virtual_machine/retruco.py
+++ src/virtual_machine/retruco.py
@@ -9,7 +9,16 @@ class Retruco(tk.Tk):
         self.columnconfigure(1, minsize=800, weight=1)
 
         self.ucp_instructions = tk.Text(self)
+        self.ucp_instructions.config(state=tk.NORMAL)
+        self.ucp_instructions.insert(
+            tk.END, "DEFINICION DE PROGRAMA\n")
         self.ucp_instructions.config(state=tk.DISABLED)
+
+        self.stack_set_instruction = tk.Text(self)
+        self.stack_set_instruction.config(state=tk.NORMAL)
+        self.stack_set_instruction.insert(
+            tk.END, "UCP EJECUTE CON LAS SIGUIENTES CARTAS:\n")
+        self.stack_set_instruction.config(state=tk.DISABLED)
 
         # main buttons for operations
         self.fr_buttons = tk.Frame(self, relief=tk.RAISED, bd=2)
@@ -48,11 +57,12 @@ class Retruco(tk.Tk):
 
         self.fr_buttons.grid(row=0, column=0, sticky="ns")
         self.ucp_instructions.grid(row=0, column=1, sticky="nsew")
+        self.stack_set_instruction.grid(row=0, column=2, sticky="nsew")
 
         # data structures for the virtual machine
         self.opcodes = []
         self.stacks_opcodes = []
-        self.stacks_names = ["A", "B", "C"]
+        self.stacks_names = []
 
         def select_stack(self, message, type_op):
             new_window = tk.Toplevel(self)
@@ -105,7 +115,96 @@ class Retruco(tk.Tk):
             print(self.opcodes)
 
         def add_card(self):
-            pass
+            new_window = tk.Toplevel(self)
+            new_window.geometry("400x125")
+
+            if self.stacks_names == []:
+                msg_lbl = tk.Label(
+                    master=new_window, text="No hay pilas actualmente\nCree una nueva")
+                msg_lbl.pack()
+            else:
+                lbl_status = tk.Label(master=new_window, text="")
+
+                lbl_instruction = tk.Label(
+                    master=new_window, text="Seleccione Pila y Carta que añadir")
+
+                frame_options = tk.Frame(master=new_window)
+
+                stack_var = tk.StringVar(frame_options)
+                stack_var.set(self.stacks_names[0])
+                stacks_op = tk.OptionMenu(
+                    frame_options, stack_var, *self.stacks_names)
+
+                card_values = [str(i) for i in range(1, 8)] + \
+                    [str(i) for i in range(10, 13)]
+                card_types = ["ESPADA", "ORO", "BASTO", "COPA"]
+
+                position = ["BOCA ARRIBA", "BOCA ABAJO"]
+
+                value_var = tk.StringVar(frame_options)
+                value_var.set(card_values[0])
+                value_op = tk.OptionMenu(
+                    frame_options, value_var, *card_values)
+
+                type_var = tk.StringVar(frame_options)
+                type_var.set(card_types[0])
+                type_op = tk.OptionMenu(frame_options, type_var, *card_types)
+
+                pos_var = tk.StringVar(frame_options)
+                pos_var.set(position[0])
+                pos_op = tk.OptionMenu(frame_options, pos_var, *position)
+
+                btn_ok = tk.Button(new_window, text="Confirmar",
+                                   command=lambda: add_stack_opcode(self, stack_var.get(),
+                                                                    value_var.get(),
+                                                                    type_var.get(), pos_var.get(), lbl_status))
+
+                stacks_op.grid(row=0, column=0, padx=5, pady=5)
+                value_op.grid(row=0, column=1,  padx=5, pady=5)
+                type_op.grid(row=0, column=2,  padx=5, pady=5)
+                pos_op.grid(row=0, column=3,  padx=5, pady=5)
+
+                lbl_instruction.pack()
+                frame_options.pack()
+                lbl_status.pack()
+                btn_ok.pack()
+
+        def add_stack_opcode(self, stack_name, value_card, type_card, pos_card, lbl):
+            print(stack_name, type(value_card), type_card, pos_card)
+
+            # check first if the card is being used in other stack
+            opcodes_to_check = list(
+                filter(lambda s: not s.isnumeric(), self.stacks_opcodes))
+
+            for opc in opcodes_to_check:
+                for i in range(len(opc)):
+                    if opc[i] in ["O", "E", "C", "B"]:
+                        palo_index = i
+                        break
+
+                c = opc[palo_index: -1]
+
+                if c == type_card[0] + value_card:
+                    # the card is already in some stack
+                    lbl.config(text="La carta ya está en una pila")
+                    return
+
+            # if it reached here it means its a new opcode to set the stack
+            if "ARRIBA" in pos_card:
+                pos = "U"
+            else:
+                pos = "D"
+
+            opcode = str(self.stacks_names.index(
+                stack_name)) + type_card[0] + value_card + pos
+            lbl.config(
+                text="La carta se añadió a la Pila {}".format(stack_name))
+            self.stacks_opcodes.append(opcode)
+            print(self.stacks_opcodes)
+            self.stack_set_instruction.config(state=tk.NORMAL)
+            self.stack_set_instruction.insert(tk.END, "AÑADA {} DE {} {} A '{}'\n".format(
+                value_card, type_card, pos_card, stack_name))
+            self.stack_set_instruction.config(state=tk.DISABLED)
 
         def add_stack(self):
             new_window = tk.Toplevel(self)
@@ -133,7 +232,12 @@ class Retruco(tk.Tk):
 
             self.stacks_names.append(name_stack)
             lbl.config(text="Pila Creada")
-            print(self.stacks_names)
+            self.stacks_opcodes.append(
+                str(self.stacks_names.index(name_stack)))
+            self.stack_set_instruction.config(state=tk.NORMAL)
+            self.stack_set_instruction.insert(
+                tk.END, "CREE LA PILA {}\n".format(name_stack))
+            self.stack_set_instruction.config(state=tk.DISABLED)
 
 
 rt = Retruco()
